586a389fb4c66c0ccca607addecab59420ed5b24
ivry12 <80361778+ivry12@users.noreply.github.com>
2022-01-28 18:39:36 +0900

    dooranimation
    door animation modified

--- CapstoneDesign_202/Assets/windowopen.cs
+++ CapstoneDesign_202/Assets/windowopen.cs
@@ -4,38 +4,47 @@ using UnityEngine;
 
 public class windowopen : MonoBehaviour
 {
-    [SerializeField] private Animator myDoor = null;
+    [SerializeField] private Animator animator = null;
+
     [SerializeField] private bool openTrigger = false;
     [SerializeField] private bool closeTrigger = false;
 
     [SerializeField] private string doorOpen = "DoorOpen";
     [SerializeField] private string doorClose = "DoorClose";
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter (Collider other)
     {
-        if (other.CompareTag("Player"))
+        Debug.Log("안됨");
+        if (!other.CompareTag("Player"))
         {
-            if (openTrigger)
-            {
-                myDoor.Play(doorOpen, 0, 0.0f);
-                gameObject.SetActive(false);
-            }
-            else if (closeTrigger)
-            {
-                myDoor.Play(doorClose, 0, 0.0f);
-                gameObject.SetActive(false);
-            }
+            animator.SetBool("isOpening",true);
+            Debug.Log("첫번째 if문에 들어옴");
+            //return;
+        }
+        if (openTrigger)
+        {
+            Debug.Log("두번째 if문에 들어옴");
+
+            animator.Play(doorOpen, 0, 0.0f);
+            gameObject.SetActive(false);
+        }
+
+        else if (closeTrigger)
+        {
+            Debug.Log("else if 문에 들어옴");
+
+            animator.Play(doorClose, 0, 0.0f);
+            gameObject.SetActive(false);
         }
     }
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerExit (Collider other)
     {
-        
+        if (!other.CompareTag("Player"))
+        {
+            animator.SetBool("isOpening", false);
+            Debug.Log("OnTriggerExit첫번째 if문에 들어옴");
+            //return;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

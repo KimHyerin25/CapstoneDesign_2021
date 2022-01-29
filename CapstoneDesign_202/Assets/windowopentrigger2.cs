535f4a478b331147fe5fb8475b474de968ec2c0e
ivry12 <80361778+ivry12@users.noreply.github.com>
2022-01-29 13:42:21 +0900

    door animation
    door animation add

---
+++ CapstoneDesign_202/Assets/windowopentrigger2.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class windowopentrigger2 : MonoBehaviour
+{
+    [SerializeField] private Animator animator = null;
+
+    [SerializeField] private bool openTrigger = false;
+    [SerializeField] private bool closeTrigger = false;
+
+    [SerializeField] private string doorOpen = "DoorOpen";
+    [SerializeField] private string doorClose = "DoorClose";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("안됨");
+        if (!other.CompareTag("Player"))
+        {
+            animator.SetBool("isOpening", true);
+            Debug.Log("첫번째 if문에 들어옴");
+           
+        }
+        if (openTrigger)
+        {
+            Debug.Log("두번째 if문에 들어옴");
+
+            animator.Play(doorOpen, 0, 0.0f);
+            gameObject.SetActive(false);
+        }
+
+        else if (closeTrigger)
+        {
+            Debug.Log("else if 문에 들어옴");
+
+            animator.Play(doorClose, 0, 0.0f);
+            gameObject.SetActive(false);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            animator.SetBool("isOpening", false);
+            Debug.Log("OnTriggerExit첫번째 if문에 들어옴");
+        
+        }
+    }
+
+}

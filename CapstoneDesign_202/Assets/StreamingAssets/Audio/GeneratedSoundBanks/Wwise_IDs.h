db35a9f6ed2b5c42f9942b199e0adf91e3757869
hearenzo <hearenzo@naver.com>
2022-02-07 15:29:08 +0900

    Wind add
    wind add

--- CapstoneDesign_202/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ CapstoneDesign_202/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,12 +14,16 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_AMB_SUBURB_SOLO_BIRD_CALL__EARLY_MORNING__DISTANT_TRAFFIC_PASSBYS__MONTREAL__CANADA__LOOP = 1882474850U;
+        static const AkUniqueID PLAY_WIND_CHIMES_SOUND_EFFECT__FREE_ = 988416763U;
+        static const AkUniqueID PLAY_WIND_IN_PINE_TREES___SOUND_EFFECT = 3483543033U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID MAIN_AMBIENCE = 72377605U;
+        static const AkUniqueID WIND_CHIMES = 1277414221U;
+        static const AkUniqueID WIND_IN_PINE_TREES = 4163747318U;
     } // namespace BANKS
 
     namespace BUSSES

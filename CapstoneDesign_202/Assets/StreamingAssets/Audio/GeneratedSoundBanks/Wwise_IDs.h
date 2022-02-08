21dc241a05b24aa4710c7000db832b99b5904f80
hearenzo <hearenzo@naver.com>
2022-02-08 14:41:42 +0900

    이걸로 한다고 합니다

--- CapstoneDesign_202/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ CapstoneDesign_202/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,6 +14,7 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_AMB_SUBURB_SOLO_BIRD_CALL__EARLY_MORNING__DISTANT_TRAFFIC_PASSBYS__MONTREAL__CANADA__LOOP = 1882474850U;
+        static const AkUniqueID PLAY_MEDITATION = 2740123722U;
         static const AkUniqueID PLAY_WIND_CHIMES_SOUND_EFFECT__FREE_ = 988416763U;
         static const AkUniqueID PLAY_WIND_IN_PINE_TREES___SOUND_EFFECT = 3483543033U;
     } // namespace EVENTS
@@ -22,6 +23,7 @@ namespace AK
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID MAIN_AMBIENCE = 72377605U;
+        static const AkUniqueID MAINHALL_MUSIC = 384319659U;
         static const AkUniqueID WIND_CHIMES = 1277414221U;
         static const AkUniqueID WIND_IN_PINE_TREES = 4163747318U;
     } // namespace BANKS
